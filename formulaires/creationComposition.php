4283bdc1738f19722809d29e6765dda742e0c1c0
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-08-02 20:02:06 +0200

    Chasse aux bugs !

--- formulaires/creationComposition.php
+++ formulaires/creationComposition.php
@@ -12,13 +12,13 @@ if ($_GET) {
 
 <article class="">
   <p> <?php if (isset($message1)) { echo $message1; } if(isset($message2)) { echo $message2; }   ?> </p>
-  <form class="conteneurFlex" action="formulaires/record/composition.php" method="post" enctype="multipart/form-data">
+  <form class="" action="formulaires/record/composition.php" method="post" enctype="multipart/form-data">
     <label for="nomC">Nom de votre composition :</label>
     <input id="nomC" class="sizeInpute" type="text" name="nomComposition" placeholder="Nom de votre composition">
     <label for="prixC">Prix de votre composition :</label>
     <input class="sizeInpute" id="prixC" type="number" step="0.01" name="prixComposition" placeholder="Prix de votre composition en €">
     <label for="imageC">Image de votre composition :</label>
     <input class="sizeInpute" id="imageC" type="file" name="image" accept="image/png, image/jpeg">
-    <button type="submit" name="button">Enregistrer</button>
+    <button class="rec" type="submit" name="button">Enregistrer</button>
   </form>
 </article>
